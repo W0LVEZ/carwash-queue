f040828a1222b0b926d3be34dd4768cf10297238
W0LVEZ <133645225+W0LVEZ@users.noreply.github.com>
2026-01-07 16:56:15 +0800

    added rendering at customer

--- customer.js
+++ customer.js
@@ -1,12 +1,13 @@
-let customerQueue = [];
-
 document.getElementById("queueForm").addEventListener("submit", function (event) {
     event.preventDefault();
+    const customerQueue = getQueue();
     const formValues = getFormValues();
-    const duplicate = checkDuplicate(formValues);
+    const duplicate = checkDuplicate(customerQueue, formValues);
     let queuePosition; 
+    //-1 not Found, 0 Found
     if (duplicate === -1) {
         customerQueue.push(formValues);
+        saveQueue(customerQueue);
         queuePosition = customerQueue.length;
     } else {
         queuePosition = duplicate + 1;
@@ -29,8 +30,28 @@ function getFormValues() {
     return customers;
 }
 
-function checkDuplicate(formValues) {
-    const findPlateCar = customerQueue.findIndex(customer => 
-        customer.plate === formValues.plate && customer.car === formValues.car);
-    return findPlateCar;
+function checkDuplicate(customerQueue, formValues) {
+    //find index returns: -1 not in queue, 0,1,2,... waiting
+    return customerQueue.findIndex(customer => 
+        customer.plate === formValues.plate && 
+        customer.car === formValues.car);
+}
+
+function renderNowServing() {
+    const nowServing = getNowServing();
+    const nowServingCustomers = document.getElementById('nowServing');
+    nowServingCustomers.textContent = '';
+
+    if (nowServing.length === 0) {
+        nowServingCustomers.textContent = 'No car to wash!'
+    }
+
+    nowServing.forEach((customer, index) => {
+    const nowServingElement = document.createElement('div');
+
+    nowServingElement.textContent =
+        `Slot ${index + 1}: ${customer.plate} (${customer.car})`;
+
+    nowServingCustomers.appendChild(nowServingElement)
+    });
 }
